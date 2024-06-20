12a41043670f668e5dc28318394ab046da5f0b21
GalinSavov <lemonkiller34@gmail.com>
2024-06-20 15:45:17 +0200

    Added an explosion particle effect

--- Laser Defender/Assets/Scripts/Health.cs
+++ Laser Defender/Assets/Scripts/Health.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth = 100f;
+    [SerializeField] private ParticleSystem explosionParticles = null;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -12,18 +13,29 @@ public class Health : MonoBehaviour
         if (damageDealer)
         {
             damageDealer.Destroy();
+            PlayExplosion();
             DealDamage(damageDealer.GetDamage());
         }
     }
 
     private void DealDamage(int amount)
     {
+        
         startingHealth -= amount;
 
         if (startingHealth <= 0)
             Destroy(gameObject);
 
         
+    }
+    private void PlayExplosion()
+    {
+       if (explosionParticles != null)
+        {
+            ParticleSystem effect = Instantiate(explosionParticles, transform.position, Quaternion.identity);
+            effect.Play();
+            Destroy(effect.gameObject,effect.main.duration);
+        }
     }
     
 }
